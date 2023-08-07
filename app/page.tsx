3c2dbd5d12ed351d9c0af28ccbcf9c44e926c4e4
anacatarinafg <anafrontenddeveloper@gmail.com>
2023-08-07 23:07:21 +0100

    adding more space

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default async function Index() {
               <Link
                 href={`/post/${post.slug.current}`}
                 prefetch
-                className="space-y-3 xl:col-span-4"
+                className="space-y-5 xl:col-span-4"
               >
                 <div>
                   <h3 className="text-2xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100">
